3603d2f2dddf18cbdcbba6af3b93951b3ff6dbaf
Denis Ryzhkov <denisr@denisr.com>
2016-09-23 12:34:02 +0300

    v0.2.0: Added "dtid" and tests.

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 uqid.py
 =======
 
-Returns unique id from random chars.  
-Default is 64 chars 0-9a-zA-Z aka base62.
-
-Usage:
+uqid
+----
 
     pip install uqid
     from uqid import uqid, digits
@@ -12,6 +10,46 @@ Usage:
     print(uqid()) # e7ljCIs4grYZ9tOv6bpX3TQIA7FyA23umzGjDcIjHff2XwF2ziAw56B7S3EdOuCA
     print(uqid(4, digits)) # 4982
 
-uqid.py version 0.1.0  
-Copyright (C) 2015 by Denis Ryzhkov <denisr@denisr.com>  
+* Returns unique id from random chars.
+* Default is 64 chars 0-9a-zA-Z aka base62.
+
+dtid
+----
+
+    pip install uqid
+    from uqid import dtid
+    print(dtid())
+
+    YYYYMMDDhhmmssmicrosrnd4
+    20161231235959123456uN1q
+
+* 24 bytes by default - as str(ObjectId()).
+* But human-friendly - unlike ObjectId.
+* Microsecond-precise UTC timestamp - unlike second-precise ObjectId().
+* May extract datetime_from_dtid().
+* May be used as a log timestamp as is.
+* Alphabetically ordered.
+* Safe chars 0-9a-zA-Z - unlike binary ObjectId() leading to "ValueError: Invalid UTF-8 sequence length" if added to JSON without str().
+* base62^4 is over 14M uniques per microsecond.
+* dtid(24) is 2x faster than uqid(24) but 2x slower than str(ObjectId()).
+
+tests
+-----
+
+    python uqid.py
+
+    Length: 24
+    uqid(24): SdGksfuUJAmDGz7suDzgJYr4
+    dtid(24): 20160923092803852711Pqci
+    str(ObjectId()): 57e4f5a39c5915591b593563
+    Iterations: 1000000
+    uqid(24) seconds: 11.457604
+    dtid(24) seconds: 5.997643
+    str(ObjectId()) seconds: 2.715093
+    uqid(24) duplicates: 0
+    dtid(24) duplicates: 0
+    str(ObjectId()) duplicates: 0
+
+uqid.py version 0.2.0  
+Copyright (C) 2015-2015 by Denis Ryzhkov <denisr@denisr.com>  
 MIT License, see http://opensource.org/licenses/MIT
--- uqid.py
+++ uqid.py
@@ -1,29 +1,93 @@
 """
-Returns unique id from random chars.
-Default is 64 chars 0-9a-zA-Z aka base62.
+uqid.py version 0.2.0
+https://github.com/denis-ryzhkov/uqidpy
 
-Usage:
-    pip install uqid
-    from uqid import uqid, digits
-
-    print(uqid()) # e7ljCIs4grYZ9tOv6bpX3TQIA7FyA23umzGjDcIjHff2XwF2ziAw56B7S3EdOuCA
-    print(uqid(4, digits)) # 4982
-
-uqid.py version 0.1.0
-Copyright (C) 2015 by Denis Ryzhkov <denisr@denisr.com>
+Copyright (C) 2015-2016 by Denis Ryzhkov <denisr@denisr.com>
 MIT License, see http://opensource.org/licenses/MIT
 """
 
-#### import
+### import
 
+from datetime import datetime
 from random import choice
 
-#### chars
+### chars
 
 digits = '0123456789'
 base62 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-#### uqid
+### uqid
 
 def uqid(length=64, chars=base62):
     return ''.join(choice(chars) for i in xrange(length))
+
+### dtid
+
+_dt_format = '%Y%m%d%H%M%S%f'
+_dt_length = 20
+
+def dtid(length=24, chars=base62):
+    s = datetime.utcnow().strftime(_dt_format)
+    if length > _dt_length:
+        return s + uqid(length - _dt_length, chars)
+    return s[:length]
+
+### datetime_from_dtid
+
+def datetime_from_dtid(s):
+    return datetime.strptime(s[:_dt_length], _dt_format)
+
+### tests
+
+def tests():
+
+    from time import time
+
+    try:
+        from bson import ObjectId
+    except ImportError:
+        ObjectId = None
+
+    length = 24
+    print('Length: {}'.format(length))
+
+    print('uqid({}): {}'.format(length, uqid(length)))
+    print('dtid({}): {}'.format(length, dtid(length)))
+    if ObjectId:
+        print('str(ObjectId()): {}'.format(str(ObjectId())))
+
+    assert len(uqid(length)) == length
+    assert len(dtid(length)) == length
+    assert (datetime.utcnow() - datetime_from_dtid(dtid())).total_seconds() < 0.1
+
+    N = 1000*1000
+    print('Iterations: {}'.format(N))
+
+    start = time()
+    for _ in xrange(N):
+        uqid(length)
+    print('uqid({}) seconds: {:.6f}'.format(length, time() - start))
+
+    start = time()
+    for _ in xrange(N):
+        dtid(length)
+    print('dtid({}) seconds: {:.6f}'.format(length, time() - start))
+
+    if ObjectId:
+        start = time()
+        for _ in xrange(N):
+            str(ObjectId())
+        print('str(ObjectId()) seconds: {:.6f}'.format(time() - start))
+
+    U = len(set(uqid(length) for _ in xrange(N)))
+    print('uqid({}) duplicates: {}'.format(length, N - U))
+
+    U = len(set(dtid(length) for _ in xrange(N)))
+    print('dtid({}) duplicates: {}'.format(length, N - U))
+
+    if ObjectId:
+        U = len(set(str(ObjectId()) for _ in xrange(N)))
+        print('str(ObjectId()) duplicates: {}'.format(N - U))
+
+if __name__ == '__main__':
+    tests()
